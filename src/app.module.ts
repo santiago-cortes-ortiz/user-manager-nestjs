0e4f919f7321635521086af210cbd24b474b5855
jeiss <jeissoncortes02@outlook.com>
2023-01-31 22:35:11 -0500

    se adiciona los parametros de conexion de la base dde datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'tbl_users',
+      entities: [],
+      synchronize: true,
+    })],
   controllers: [],
   providers: [],
 })
