2141132b91b9a7394bd6ce696fd0d2c84ef1a190
jeiss <jeissoncortes02@outlook.com>
2023-02-05 20:51:53 -0500

    se agrega la logica de negocio del user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./model/user.model";
 
-@Injectable({})
+@Injectable()
 export class UserService{
-    
+    constructor(
+        @InjectRepository(User) 
+        private _userRepository: Repository<User>
+    ) {}
+
+    findAll(): Promise<User[]>{
+        return this._userRepository.find();
+    }
+
+    findById(id: number): Promise<User> {
+        return this._userRepository.findOneBy({id});
+    }
+
+    save(user: User): Promise<User> {
+        return this._userRepository.save(user);
+    }
+
+    async delete(id: number): Promise<void> {
+        await this._userRepository.delete(id);
+    }
+
 }
